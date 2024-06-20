00df615181cbb629c763067d806d894297435ee7
Katsumata <naoyakatsumata0708@gmail.com>
2024-06-20 19:23:18 +0900

    add Model

--- src/resources/views/allshop.blade.php
+++ src/resources/views/allshop.blade.php
@@ -7,86 +7,92 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/allshop.css') }}">
 </head>
-<body>
-    <div class="flex">
-        <div class="p-10">
-            @auth
-                <div id="loginMenu" class="w-[40px] h-[40px] bg-blue-500 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
-                    <div class="w-[70%] mx-auto">
-                        <div class="absolute top-[10px] w-[50%] h-[2px] bg-white mx-auto"></div>
-                        <div class="absolute top-[20px] w-[70%] h-[2px] bg-white"></div>
-                        <div class="absolute top-[30px] w-[20%] h-[2px] bg-white"></div>
+<body class="bg-zinc-100">
+    <!-- アイコン&タイトル -->
+    <header class="fixed w-full bg-zinc-100">
+        <div class="flex">
+            <div class="pl-16 py-6">
+                @auth
+                    <div id="loginMenu" class="w-[40px] h-[40px] bg-blue-600 rounded-[5px] shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
+                        <div class="w-[70%] mx-auto">
+                            <div class="absolute top-[10px] w-[50%] h-[1px] bg-white mx-auto"></div>
+                            <div class="absolute top-[20px] w-[70%] h-[1px] bg-white"></div>
+                            <div class="absolute top-[30px] w-[20%] h-[1px] bg-white"></div>
+                        </div>
                     </div>
-                </div>
-            @else
-                <div id="gestMenu" class="w-[40px] h-[40px] bg-blue-500 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
-                    <div class="w-[70%] mx-auto">
-                        <div class="absolute top-[10px] w-[50%] h-[2px] bg-white mx-auto"></div>
-                        <div class="absolute top-[20px] w-[70%] h-[2px] bg-white"></div>
-                        <div class="absolute top-[30px] w-[20%] h-[2px] bg-white"></div>
+                @else
+                    <div id="gestMenu" class="w-[40px] h-[40px] bg-blue-600 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
+                        <div class="w-[70%] mx-auto">
+                            <div class="absolute top-[10px] w-[50%] h-[1px] bg-white mx-auto"></div>
+                            <div class="absolute top-[20px] w-[70%] h-[1px] bg-white"></div>
+                            <div class="absolute top-[30px] w-[20%] h-[1px] bg-white"></div>
+                        </div>
                     </div>
-                </div>
-            @endauth
+                @endauth
+            </div>
+            <div class="px-16 py-6 text-4xl text-blue-600 font-bold">Rese</div>
         </div>
-        <div class="px-8 py-[40px] text-4xl text-blue-500">Rese</div>
-    </div>
-<!-- @if (Route::has('login'))
-    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-        @auth
-            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-            <p>test</p>
-        @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-            @endif
-        @endauth
-    </div>
-@endif -->
-<div class="absolute w-[90%] mx-auto top-[40px] left-[40px]">
-    <div id="menuBg" class="w-full h-full fixed top-[0px] left-[0px] bottom-[0px] bg-white"></div>
-    <div id="close" class="w-[40px] h-[40px] bg-blue-500 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
-        <div class="w-[70%] mx-auto">
-            <div class="absolute top-[10px] w-[50%] h-[2px] bg-white mx-auto"></div>
-            <div class="absolute top-[20px] w-[70%] h-[2px] bg-white"></div>
-            <div class="absolute top-[30px] w-[20%] h-[2px] bg-white"></div>
-        </div>
-    </div>
-    <div id="gestContainer" class="absolute w-full my-32 text-center">
-        <ul>
-            <li class="mb-2 text-2xl text-blue-500"><a href="/">Home</a></li>
-            <li class="mb-2 text-2xl text-blue-500"><a href="/register">Registration</a></li>
-            <li class="mb-2 text-2xl text-blue-500"><a href="/login">Login</a></li>
-        </ul>
-    </div>
-</div>
+    </header>
 
-<div class="absolute w-[90%] mx-auto top-[40px] left-[40px]">
-    <div id="menuBg" class="w-full h-full fixed top-[0px] left-[0px] bottom-[0px] bg-white"></div>
-    <div id="close" class="w-[40px] h-[40px] bg-blue-500 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
-        <div class="w-[70%] mx-auto">
-            <div class="absolute top-[10px] w-[50%] h-[2px] bg-white mx-auto"></div>
-            <div class="absolute top-[20px] w-[70%] h-[2px] bg-white"></div>
-            <div class="absolute top-[30px] w-[20%] h-[2px] bg-white"></div>
+    <div class="py-24 mx-auto w-[90%]">
+        <div class="flex flex-wrap h-[200px]">
+            <!-- 店カード -->
+            <div class="flex-column break-words w-[24%] mx-[0.5%] bg-white rounded-[10px]">
+                <img class="object-cover w-full h-1/2 rounded-t-[10px]" src="{{asset('img/italian.jpg')}}">
+                <div class="m-4">仙人</div>
+                
+            </div>
+        </div>
+
+    </div>
+
+    <!-- 未ログイン時メニュー -->
+    <div class="absolute w-[90%] mx-auto top-[40px] left-[40px]">
+        <div id="menuBg" class="w-full h-full fixed top-[0px] left-[0px] bottom-[0px] bg-white"></div>
+        <div id="close" class="w-[40px] h-[40px] bg-blue-500 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
+            <div class="w-[70%] mx-auto">
+                <div class="absolute top-[10px] w-[50%] h-[2px] bg-white mx-auto"></div>
+                <div class="absolute top-[20px] w-[70%] h-[2px] bg-white"></div>
+                <div class="absolute top-[30px] w-[20%] h-[2px] bg-white"></div>
+            </div>
+        </div>
+        <div id="gestContainer" class="absolute w-full my-32 text-center">
+            <ul>
+                <li class="mb-2 text-2xl text-blue-500"><a href="/">Home</a></li>
+                <li class="mb-2 text-2xl text-blue-500"><a href="/register">Registration</a></li>
+                <li class="mb-2 text-2xl text-blue-500"><a href="/login">Login</a></li>
+            </ul>
         </div>
     </div>
-    <div id="loginContainer" class="absolute w-full my-32 text-center">
-        <ul>
-            <li class="mb-2 text-2xl text-blue-500"><a href="/">Home</a></li>
-            <li class="mb-2"><form class="text-2xl text-blue-500" method="POST" action="{{ route('logout') }}">
-                            @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form></li>
-            <li class="mb-2 text-2xl text-blue-500"><a href="">Mypage</a></li>
-        </ul>
+
+    <!-- ログイン時メニュー -->
+    <div class="absolute w-[90%] mx-auto top-[40px] left-[40px]">
+        <div id="menuBg" class="w-full h-full fixed top-[0px] left-[0px] bottom-[0px] bg-white"></div>
+        <div id="close" class="w-[40px] h-[40px] bg-blue-500 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
+            <div class="w-[70%] mx-auto">
+                <div class="absolute top-[10px] w-[50%] h-[2px] bg-white mx-auto"></div>
+                <div class="absolute top-[20px] w-[70%] h-[2px] bg-white"></div>
+                <div class="absolute top-[30px] w-[20%] h-[2px] bg-white"></div>
+            </div>
+        </div>
+        <div id="loginContainer" class="absolute w-full my-32 text-center">
+            <ul>
+                <li class="mb-2 text-2xl text-blue-500"><a href="/">Home</a></li>
+                <li class="mb-2"><form class="text-2xl text-blue-500" method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form></li>
+                <li class="mb-2 text-2xl text-blue-500"><a href="">Mypage</a></li>
+            </ul>
+        </div>
     </div>
-</div>
-<script src="{{ asset('js/loginmenu.js') }}"></script>
-<script src="{{ asset('js/gestmenu.js') }}"></script>
+    <script src="{{ asset('js/loginmenu.js') }}"></script>
+    <script src="{{ asset('js/gestmenu.js') }}"></script>
 </body>
 </html>
