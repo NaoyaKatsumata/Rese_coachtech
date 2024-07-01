318e8b5a41ebaeed51afb0d931ebc8b4cf51fcf3
Katsumata <naoyakatsumata0708@gmail.com>
2024-07-01 23:58:31 +0900

    予約完了ページのメニューウィンドの表示修正完了

--- src/resources/views/done.blade.php
+++ src/resources/views/done.blade.php
@@ -14,7 +14,7 @@
         <div class="flex mx-auto my-6 justify-between">
             <div class="flex">
                 <div class="">
-                    <div id="gestMenu" class="relative w-[40px] h-[40px] bg-blue-600 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
+                    <div id="loginMenu" class="relative w-[40px] h-[40px] bg-blue-600 rounded-[5px] fixed shadow-[3px_3px_0px_0px_rgba(0,0,0,0.3)]">
                         <div class="w-[70%] mx-auto">
                             <div class="absolute top-[10px] w-[50%] h-[1px] bg-white mx-auto"></div>
                             <div class="absolute top-[20px] w-[70%] h-[1px] bg-white"></div>
@@ -51,16 +51,38 @@
                 <div class="absolute top-[30px] w-[20%] h-[1px] bg-white"></div>
             </div>
         </div>
-        <div id="gestContainer" class="absolute w-full my-32 text-center">
-            <ul>
-                <li class="mb-2 text-2xl text-blue-500"><a href="/">Home</a></li>
-                <li class="mb-2 text-2xl text-blue-500"><a href="/register">Registration</a></li>
-                <li class="mb-2 text-2xl text-blue-500"><a href="/login">Login</a></li>
+        <div id="loginContainer" class="absolute w-full my-32 text-center">
+        <ul>
+                <li class="mb-2 text-2xl text-blue-500"><form class="text-2xl text-blue-500" method="POST" action="/">
+                                @csrf
+                                @method('PUT')
+                                <input type="hidden" name="userId" value="{{$userId}}">
+                                <input type="hidden" name="shopId" value="">
+                                <input type="hidden" name="area" value="All shop">
+                                <input type="hidden" name="category" value="">
+                                <input type="submit" value="Home">
+                            </form></li>
+                <li class="mb-2"><form class="text-2xl text-blue-500" method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form></li>
+                <li class="mb-2 text-2xl text-blue-500"><form class="text-2xl text-blue-500" method="POST" action="/mypage">
+                                @csrf
+                                @auth
+                                <input type="hidden" value="{{Auth::user()->id}}">
+                                @endauth
+                                <input type="submit" value="My page">
+                            </form></li>
             </ul>
         </div>
     </div>
 
     <!-- javascript読み込み -->
-    <script src="{{ asset('js/gestmenu.js') }}"></script>
+    <script src="{{ asset('js/loginmenu.js') }}"></script>
 </body>
 </html>
